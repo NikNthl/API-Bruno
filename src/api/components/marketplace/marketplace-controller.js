9202597abdbabe99b0ec77fedd6e264b792dc10f
OctoNik <nikolaus.535230113@stu.untar.ac.id>
2024-05-05 23:00:17 +0700

    marketplace-controller

---
+++ src/api/components/marketplace/marketplace-controller.js
@@ -0,0 +1,109 @@
+const marketplaceService = require('./marketplace-service');
+const { errorResponder, errorTypes } = require('../../../core/errors');
+
+async function getMarketplaces(request, response, next) {
+  try {
+    const marketplaces = await marketplaceService.getMarketplaces();
+    return response.status(200).json(marketplaces);
+  } catch (error) {
+    return next(error);
+  }
+}
+
+async function getMarketplace(request, response, next) {
+  try {
+    const marketplace = await marketplaceService.getMarketplace(
+      request.params.id
+    );
+
+    if (!marketplace) {
+      throw errorResponder(
+        errorTypes.UNPROCESSABLE_ENTITY,
+        'Unknown marketplace'
+      );
+    }
+
+    return response.status(200).json(marketplace);
+  } catch (error) {
+    return next(error);
+  }
+}
+
+async function createMarketplace(request, response, next) {
+  try {
+    const { nama_barang, harga_barang, stok_barang } = request.body;
+
+    const success = await marketplaceService.createMarketplace(
+      nama_barang,
+      harga_barang,
+      stok_barang
+    );
+
+    if (!success) {
+      throw errorResponder(
+        errorTypes.UNPROCESSABLE_ENTITY,
+        'Failed to create item'
+      );
+    }
+
+    return response
+      .status(200)
+      .json({ nama_barang, harga_barang, stok_barang });
+  } catch (error) {
+    return next(error);
+  }
+}
+
+async function updateMarketplace(request, response, next) {
+  try {
+    const id = request.params.id;
+    const { nama_barang, harga_barang, stok_barang } = request.body;
+
+    const success = await marketplaceService.updateMarketplace(
+      id,
+      nama_barang,
+      harga_barang,
+      stok_barang
+    );
+
+    if (!success) {
+      throw errorResponder(
+        errorTypes.UNPROCESSABLE_ENTITY,
+        'Failed to update item'
+      );
+    }
+
+    return response
+      .status(200)
+      .json({ id, nama_barang, harga_barang, stok_barang });
+  } catch (error) {
+    return next(error);
+  }
+}
+
+async function deleteMarketplace(request, response, next) {
+  try {
+    const id = request.params.id;
+
+    const success = await marketplaceService.deleteMarketplace(id);
+
+    if (!success) {
+      throw errorResponder(
+        errorTypes.UNPROCESSABLE_ENTITY,
+        'Failed to delete item'
+      );
+    }
+
+    return response.status(200).json({ id });
+  } catch (error) {
+    return next(error);
+  }
+}
+
+module.exports = {
+  getMarketplaces,
+  getMarketplace,
+  createMarketplace,
+  updateMarketplace,
+  deleteMarketplace,
+};
