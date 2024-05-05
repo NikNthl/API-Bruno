dcabae0ed6b077da85b9da55915e68517d628971
OctoNik <nikolaus.535230113@stu.untar.ac.id>
2024-05-05 23:00:40 +0700

    marketplace-route

---
+++ src/api/components/marketplace/marketplace-route.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const authenticationMiddleware = require('../../middlewares/authentication-middleware');
+const celebrate = require('../../../core/celebrate-wrappers');
+const marketplaceControllers = require('./marketplace-controller');
+const marketplaceValidator = require('./marketplace-validator');
+
+const route = express.Router();
+
+module.exports = (app) => {
+  app.use('/marketplace', route);
+
+  // Get list of items
+  route.get(
+    '/',
+    authenticationMiddleware,
+    marketplaceControllers.getMarketplaces
+  );
+
+  // Create items
+  route.post(
+    '/',
+    celebrate(marketplaceValidator.createMarketplace),
+    marketplaceControllers.createMarketplace
+  );
+
+  // Get item details
+  route.get(
+    '/:id',
+    authenticationMiddleware,
+    marketplaceControllers.getMarketplace
+  );
+
+  // Update items
+  route.put(
+    '/:id',
+    celebrate(marketplaceValidator.updateMarketplace),
+    marketplaceControllers.updateMarketplace
+  );
+
+  // Delete items
+  route.delete(
+    '/:id',
+    authenticationMiddleware,
+    marketplaceControllers.deleteMarketplace
+  );
+};
